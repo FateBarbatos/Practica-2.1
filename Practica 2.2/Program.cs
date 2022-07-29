5ec2b08bf777902176ead46acb9f2c3ff72e4cc5
Fate <Fate@DESKTOP-QKB1521>
2022-07-29 09:43:23 +0200

    Segundo codigo

---
+++ Practica 2.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Practica_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FirsName = "Alcides";
+            String SecondName = "Granados";
+            int edad = 16;
+            var name = FirsName + " " + SecondName;
+            Console.WriteLine("Tu nombre es: {0} y su edad es {1}" , name, edad);
+        }
+    }
+}
