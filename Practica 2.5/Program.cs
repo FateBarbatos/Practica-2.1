aa13503ccaac4f987e2acdfeff2822ef439cc79d
Fate <Fate@DESKTOP-QKB1521>
2022-07-29 10:27:40 +0200

    quinto programa

--- Practica 2.5/Program.cs
+++ Practica 2.5/Program.cs
@@ -16,6 +16,9 @@ namespace Practica_2._5
             int division = num1 / num2;
             
             Console.WriteLine("La suma es igual a:{0}", suma);
+            Console.WriteLine("La resta es igual a {0}", resta);
+            Console.WriteLine("La multiplicacion es igual a {0}", multiplicacion);
+            Console.WriteLine("La division es igual a {0}", division);
         }
     }
 }
