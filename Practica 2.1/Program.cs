9d9b0d0d6871a66b1f5ba2e96e58c41efaff578d
Fate <Fate@DESKTOP-QKB1521>
2022-07-29 08:58:19 +0200

    Agregar archivos de proyecto.

---
+++ Practica 2.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practica_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
