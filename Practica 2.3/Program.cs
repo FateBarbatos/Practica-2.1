1edce0f6a5539d7d49e25c6259615df398c0cb9e
Fate <Fate@DESKTOP-QKB1521>
2022-07-29 10:06:28 +0200

    tercer programa

---
+++ Practica 2.3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Practica_2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1 = 52.24F;
+            double num2 = 52.24;
+            decimal num3 = 52.24M;
+
+            Console.WriteLine("{0} {1} {2}",num1,num2,num3);
+            /*float ------> 7 digitos --- 32 bits
+             * double ----> 15 - 16 digitos --- 64 bits
+             * decimal ---> 28 - 29 digitos -- 128 bits
+             */
+        }
+    }
+}
