86aa2eba12246ddbd122ce68e84426ba20a00b47
Fate <Fate@DESKTOP-QKB1521>
2022-07-29 09:23:24 +0200

    Primer programa

--- Practica 2.1/Program.cs
+++ Practica 2.1/Program.cs
@@ -6,7 +6,15 @@ namespace Practica_2._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Declaracion de variables
+
+            int num1 = 5; //variable numero 1
+            int num2 = 4; //variable numero 2
+            var total = num1 + num2; // suma de las variables
+            string name = "Enrique";
+            int edad = 27;
+            Console.WriteLine("El resultado de la suma es:" + total);
+            Console.WriteLine(name + " " + edad);
         }
     }
 }
