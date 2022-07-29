4cb8da49c52a7c10d52028affcc04597b8f38ea3
Fate <Fate@DESKTOP-QKB1521>
2022-07-29 10:24:43 +0200

    quinto programa

---
+++ Practica 2.4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Practica_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca su nombre:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Introduzca su edad:");
+            int edad = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Su nombre es {0} y tiene {1}", name, edad);
+        }
+    }
+}
---
+++ Practica 2.5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Practica_2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca el primer numero");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduzca el segundo numero");
+            int num2 = int.Parse(Console.ReadLine());
+            int suma = num1 + num2;
+            int resta = num1 - num2;
+            int multiplicacion = num1 * num2;
+            int division = num1 / num2;
+            
+            Console.WriteLine("La suma es igual a:{0}", suma);
+        }
+    }
+}
